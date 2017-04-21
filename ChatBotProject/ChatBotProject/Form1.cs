ceda857fd94d7054ed75da9f7df15bdbddd46724
GR8 <gr8_boi52@yahoo.com>
2017-04-21 13:41:58 +0400

    Added Base Code
    Initialized the project with the most basic stucture of the program so
    we can learn and modify it.

---
+++ ChatBotProject/ChatBotProject/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AIMLbot;
+
+namespace ChatBotProject
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static ChatBot bot;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bot = new ChatBot();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            outputTxt.AppendText("YOU: \n" + InputTxt.Text + Environment.NewLine);
+            outputTxt.AppendText("BOT: \n" + bot.getOutput(InputTxt.Text) + Environment.NewLine);
+        }
+    }
+}
